00756158e8b24fe856a84a1d222b9daa6cd43c02
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2015-11-25 14:13:03 +0100

    Added Test_usrc_Item_PageHandler

---
+++ Test_usrc_Item_PageHandler/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Test_usrc_Item_PageHandler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Items = new System.Windows.Forms.Panel();
+            this.usrc_Item_PageHandler = new usrc_Item_PageHandler.usrc_Item_PageHandler();
+            this.SuspendLayout();
+            // 
+            // pnl_Items
+            // 
+            this.pnl_Items.AutoScroll = true;
+            this.pnl_Items.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnl_Items.Location = new System.Drawing.Point(47, 105);
+            this.pnl_Items.Name = "pnl_Items";
+            this.pnl_Items.Size = new System.Drawing.Size(620, 457);
+            this.pnl_Items.TabIndex = 1;
+            // 
+            // usrc_Item_PageHandler
+            // 
+            this.usrc_Item_PageHandler.Location = new System.Drawing.Point(43, 24);
+            this.usrc_Item_PageHandler.Name = "usrc_Item_PageHandler";
+            this.usrc_Item_PageHandler.Size = new System.Drawing.Size(215, 51);
+            this.usrc_Item_PageHandler.TabIndex = 0;
+            this.usrc_Item_PageHandler.ShowObject += new usrc_Item_PageHandler.usrc_Item_PageHandler.delegate_ShowObject(this.usrc_Item_PageHandler_ShowObject);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 590);
+            this.Controls.Add(this.pnl_Items);
+            this.Controls.Add(this.usrc_Item_PageHandler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private usrc_Item_PageHandler.usrc_Item_PageHandler usrc_Item_PageHandler;
+        private System.Windows.Forms.Panel pnl_Items;
+    }
+}
+
---
+++ Test_usrc_Item_PageHandler/usrc_Item.cs
@@ -0,0 +1,295 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_usrc_Item_PageHandler
+{
+    public partial class usrc_Item : UserControl
+    {
+        private class ControlPos
+        {
+            public int left_pos_initial_of_button = -1;
+            public int left_pos_initial_of_nmUpDn = -1;
+            public ControlPos(Button xbtn, NumericUpDown xnmUpDown)
+            {
+                left_pos_initial_of_button = xbtn.Left;
+                left_pos_initial_of_nmUpDn = xnmUpDown.Left;
+            }
+        }
+        private class Arrangement
+        {
+            ControlPos[] m_ControlPos = new ControlPos[2];
+
+            public void Define(int index,Button xbtn,NumericUpDown xnmUpDown)
+            {
+                m_ControlPos[index] = new ControlPos(xbtn,xnmUpDown);
+            }
+
+            public void Set(int index, Button xbtn, NumericUpDown xnmUpDown, bool bVisible)
+            {
+                xbtn.Left = m_ControlPos[index].left_pos_initial_of_button;
+                xbtn.Visible = bVisible;
+                xnmUpDown.Left = m_ControlPos[index].left_pos_initial_of_nmUpDn;
+                xnmUpDown.Visible = bVisible;
+            }
+
+        }
+
+
+
+
+        public enum eMode { STOCK, FACTORY, STOCK_AND_FACTORY }
+        public eMode Mode = eMode.STOCK_AND_FACTORY;
+        public decimal ExtraDiscount = 0;
+        private decimal TotalDiscount = 0;
+        private Arrangement m_Arrangement = null;
+        private Color DefaultColor = Color.Gray;
+
+
+        public usrc_Item()
+        {
+            InitializeComponent();
+        }
+
+        bool disposed = false;
+
+        // Protected implementation of Dispose pattern. 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            this.nmUpDn_StockQuantity.ValueChanged -=  (System.EventHandler)this.nmUpDn_StockQuantity_ValueChanged;
+            this.nmUpDn_FactoryQuantity.ValueChanged -= (System.EventHandler) this.nmUpDn_FactoryQuantity_ValueChanged;
+            this.btn_Discount.Click -= (System.EventHandler)this.btn_Discount_Click;
+            this.btn_EditItem.Click -=  (System.EventHandler)this.btn_EditItem_Click;
+
+            if (disposing)
+            {
+                int i= 0;
+                for (i = Controls.Count-1;i >= 0 ;i--)
+                {
+                    Control c = Controls[i];
+                    Controls.Remove(c);
+                    c.Dispose();
+                }
+                Controls.Clear();
+            }
+
+            // Free any unmanaged objects here. 
+            //
+            disposed = true;
+            // Call base class implementation. 
+            base.Dispose(disposing);
+        }
+
+
+        public decimal dQuantity
+        {
+            get
+            {
+                switch (Mode)
+                {
+                    case eMode.STOCK_AND_FACTORY:
+                        return GetValue(nmUpDn_StockQuantity) + GetValue(nmUpDn_FactoryQuantity);
+
+                    case eMode.STOCK:
+                        return GetValue(nmUpDn_StockQuantity);
+
+                    case eMode.FACTORY:
+                        return GetValue(nmUpDn_FactoryQuantity);
+
+                    default:
+                        return 0;
+                }
+            }
+
+        }
+
+        private decimal GetValue(NumericUpDown nmUpDn_Quantity)
+        {
+            if (nmUpDn_Quantity.Visible)
+            {
+                return nmUpDn_Quantity.Value;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        internal void Init()
+        {
+        }
+
+
+        private void Set_Item_Mode(eMode xeMode)
+        {
+            Mode = xeMode;
+            switch (Mode)
+            {
+                case eMode.STOCK_AND_FACTORY:
+                    m_Arrangement.Set(0, btn_Stock, nmUpDn_StockQuantity, true);
+                    m_Arrangement.Set(1, btn_NoStock, nmUpDn_FactoryQuantity, true);
+                    this.BackColor = DefaultColor;
+                    break;
+
+                case eMode.STOCK:
+                    m_Arrangement.Set(0, btn_Stock, nmUpDn_StockQuantity, true);
+                    m_Arrangement.Set(1, btn_NoStock, nmUpDn_FactoryQuantity, false);
+                    this.BackColor = DefaultColor;
+                    break;
+
+                case eMode.FACTORY:
+                    m_Arrangement.Set(0, btn_Stock, nmUpDn_StockQuantity, false);
+                    m_Arrangement.Set(0, btn_NoStock, nmUpDn_FactoryQuantity, true);
+                    this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+                    break;
+            }
+        }
+
+
+        private void Set_btn_Discount_Text()
+        {
+        }
+
+        private void Set_txt_Price()
+        {
+        }
+
+        internal void Init_NotInStock()
+        {
+
+        }
+
+        private decimal Get_Stock_RetailPricePerUnit()
+        {
+            return 0;
+        }
+
+
+        private void btn_EditItem_Click(object sender, EventArgs e)
+        {
+            EditItem();
+        }
+
+        private bool EditItem()
+        {
+            return true;
+        }
+        private void btn_Discount_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        internal void HideStock()
+        {
+            this.btn_Stock.Visible = false;
+            this.txt_InStock.Visible = false;
+            this.nmUpDn_StockQuantity.Visible = false;
+            HideAll();
+        }
+        internal void HideFactory()
+        {
+            this.btn_NoStock.Visible = false;
+            this.nmUpDn_FactoryQuantity.Visible = false;
+            HideAll();
+        }
+
+        private void HideAll()
+        {
+            if (!this.nmUpDn_FactoryQuantity.Visible && !this.nmUpDn_StockQuantity.Visible)
+            {
+                this.lbl_ItemPrice.Visible = false;
+                this.txt_Price.Visible = false;
+                btn_Discount.Visible = false;
+            }
+            else
+            {
+                this.lbl_ItemPrice.Visible = true;
+                this.txt_Price.Visible = true;
+                btn_Discount.Visible = true;
+
+            }
+        }
+
+        internal void ShowFromStock()
+        {
+            this.btn_Stock.Visible = true;
+            this.txt_InStock.Visible = true;
+            this.nmUpDn_StockQuantity.Visible = true;
+
+            this.lbl_ItemPrice.Visible = true;
+            this.txt_Price.Visible = true;
+            btn_Discount.Visible = true;
+
+            Set_txt_Price();
+        }
+
+        internal void ShowFromFactory()
+        {
+            this.btn_NoStock.Visible = true;
+            this.nmUpDn_FactoryQuantity.Visible = true;
+            this.lbl_ItemPrice.Visible = true;
+            this.txt_Price.Visible = true;
+            btn_Discount.Visible = true;
+            Set_txt_Price();
+        }
+
+        private void nmUpDn_StockQuantity_ValueChanged(object sender, EventArgs e)
+        {
+            if (this.nmUpDn_StockQuantity.Value != 0)
+            {
+                this.btn_Stock.Enabled = true;
+            }
+            else
+            {
+                this.btn_Stock.Enabled = false;
+            }
+
+            Set_txt_Price();
+        }
+
+        private void nmUpDn_FactoryQuantity_ValueChanged(object sender, EventArgs e)
+        {
+            if (this.nmUpDn_FactoryQuantity.Value != 0)
+            {
+                this.btn_NoStock.Enabled = true;
+            }
+            else
+            {
+                this.btn_NoStock.Enabled = false;
+            }
+            Set_txt_Price();
+        }
+
+
+
+        internal void ClearAll()
+        {
+            this.nmUpDn_StockQuantity.ValueChanged -= (System.EventHandler)this.nmUpDn_StockQuantity_ValueChanged;
+            this.nmUpDn_FactoryQuantity.ValueChanged -= (System.EventHandler)this.nmUpDn_FactoryQuantity_ValueChanged;
+            this.btn_Discount.Click -= (System.EventHandler)this.btn_Discount_Click;
+            this.btn_EditItem.Click -= (System.EventHandler)this.btn_EditItem_Click;
+            foreach (Control ctrl in Controls)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+
+                ctrl.Dispose();
+                if (!ctrl.IsDisposed)
+                {
+                    MessageBox.Show("!ctrl.IsDisposed");
+                }
+            }
+        }
+    }
+}
