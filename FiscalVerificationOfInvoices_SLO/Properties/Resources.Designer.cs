eee4d3d2fcd98b31b21512e5bb7d29260bbee234
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2015-12-03 16:40:20 +0100

    Missing Icon added

--- FiscalVerificationOfInvoices_SLO/Properties/Resources.Designer.cs
+++ FiscalVerificationOfInvoices_SLO/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace FiscalVerificationOfInvoices_SLO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap TAX_Office_Connection_OK1 {
+            get {
+                object obj = ResourceManager.GetObject("TAX_Office_Connection_OK1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap TAX_Office_Transmission_1 {
             get {
                 object obj = ResourceManager.GetObject("TAX_Office_Transmission_1", resourceCulture);
