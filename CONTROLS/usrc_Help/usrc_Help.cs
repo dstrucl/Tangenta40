51ffa4ed671e7cefe18f026f1516eee690a9d691
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2015-12-31 19:56:10 +0100

    some source added

---
+++ CONTROLS/usrc_Help/usrc_Help.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace usrc_Help
+{
+    public partial class usrc_Help: UserControl
+    {
+        public usrc_Help()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Help_Click(object sender, EventArgs e)
+        {
+            Form_Help hlp_dlg = new Form_Help();
+            hlp_dlg.ShowDialog();
+
+        }
+    }
+}
