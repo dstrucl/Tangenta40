98b4e61a893f02591326c532941797563e25385c
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2016-01-15 19:04:16 +0100

    Added resources

---
+++ SLO_FISCAL/FiscalVerificationOfInvoices_SLO/FormWait.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FiscalVerificationOfInvoices_SLO
+{
+    partial class FormWait
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWait));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TmrStart = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prenašam podatke na FURS";
+            // 
+            // TmrStart
+            // 
+            this.TmrStart.Interval = 1000;
+            this.TmrStart.Tick += new System.EventHandler(this.TmrStart_Tick);
+            // 
+            // FormWait
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(474, 144);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormWait";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Prenašam podatke na DURS";
+            this.Load += new System.EventHandler(this.FormWait_Load);
+            this.Shown += new System.EventHandler(this.FormWait_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer TmrStart;
+    }
+}
---
+++ SLO_FISCAL/FiscalVerificationOfInvoices_SLO/FormWait.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiscalVerificationOfInvoices_SLO
+{
+    public partial class FormWait : Form
+    {
+
+        private usrc_FVI_SLO m_usrc_FVI_SLO;
+        private Thread_FVI_Message m_msg;
+
+        /****** For DEBUG & TEST PURPOSES ***/
+        usrc_DEBUG_MessagePreview m_DEBUG_MessagePreview = null;
+        int iForm_Default_Height = -1;
+        int iForm_Default_Width = -1;
+        FormBorderStyle default_FormBorderStyle = FormBorderStyle.None;
+        /****** End For DEBUG & TEST PURPOSES ***/
+
+        public FormWait(usrc_FVI_SLO Parent, Thread_FVI_Message msg )
+        {
+            InitializeComponent();
+
+           
+
+            m_usrc_FVI_SLO = Parent;
+            m_msg = msg;
+
+            /****** For DEBUG & TEST PURPOSES ***/
+            if (m_usrc_FVI_SLO.DEBUG)
+            {
+                default_FormBorderStyle = this.FormBorderStyle;
+                iForm_Default_Height = this.Height;
+                iForm_Default_Width = this.Width;
+                Show_usrc_DEBUG_MessagePreview();
+            }
+            /****** End For DEBUG & TEST PURPOSES ***/
+
+
+            //  this.Location = m_parent.ParentForm.Location;
+            //   Point p = new Point(m_parent.ParentForm.Width / 2 - this.Width / 2, m_parent.ParentForm.Height / 2 - this.Height / 2);
+            //    this.Location = p;
+
+        }
+
+        
+        /****** For DEBUG & TEST PURPOSES ***/
+        private void Show_usrc_DEBUG_MessagePreview()
+        {
+            if (m_DEBUG_MessagePreview != null)
+            {
+                this.Controls.Remove(m_DEBUG_MessagePreview);
+                m_DEBUG_MessagePreview.Dispose();
+                m_DEBUG_MessagePreview = null;
+            }
+
+            if (m_DEBUG_MessagePreview == null)
+            {
+                m_DEBUG_MessagePreview = new usrc_DEBUG_MessagePreview(m_msg);
+                this.Controls.Add(m_DEBUG_MessagePreview);
+                this.Height = iForm_Default_Height + m_DEBUG_MessagePreview.Height + 10;
+                if (this.Width < m_DEBUG_MessagePreview.Width + 10)
+                {
+                    this.Width = m_DEBUG_MessagePreview.Width + 10;
+                }
+                m_DEBUG_MessagePreview.Top = iForm_Default_Height;
+                m_DEBUG_MessagePreview.Left = 5;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                m_DEBUG_MessagePreview.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+                m_DEBUG_MessagePreview.PostMessage += M_DEBUG_MessagePreview_PostMessage;
+
+            }
+            
+        }
+
+        private void M_DEBUG_MessagePreview_PostMessage()
+        {
+            PostMessage();
+        }
+
+        /****** End For DEBUG & TEST PURPOSES ***/
+
+
+
+
+        private void FormWait_Load(object sender, EventArgs e)
+        {
+            if (m_usrc_FVI_SLO.DEBUG)
+            {
+                /* PostMessage(); will be done whe you click on button [PostMessage --> Thread_FVI_Message] 
+                   which isa button  in  M_DEBUG_MessagePreview usercontrol (file:usrc_DEBUG_MessagePreview.cs)
+                   When you click this button it triggers M_DEBUG_MessagePreview_PostMessage event on this form!
+                */
+            }
+            else
+            {
+                /* PostMessage(); will be done in TmrStart_Tick */
+                TmrStart.Enabled = true;
+            }
+        }
+
+        private void FormWait_Shown(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void PostMessage()
+        {
+            m_usrc_FVI_SLO.RetFromWaitForm = m_usrc_FVI_SLO.thread_fvi.message_box.Post(m_msg);
+        }
+
+        private void TmrStart_Tick(object sender, EventArgs e)
+        {
+            TmrStart.Enabled = false;
+
+            Refresh();
+
+            PostMessage();
+        }
+    }
+}
