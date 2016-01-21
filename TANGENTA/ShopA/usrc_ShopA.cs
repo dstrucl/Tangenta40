f5aae9dcd23b9bddc878f7b830c8819643a685be
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2016-01-21 21:27:17 +0100

    Add

---
+++ TANGENTA/ShopA/usrc_ShopA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InvoiceDB;
+
+namespace ShopA
+{
+    public partial class usrc_ShopA: UserControl
+    {
+        ShopBC m_ShopBC = null;
+        DBTablesAndColumnNames DBtcn = null;
+        public usrc_ShopA()
+        {
+            InitializeComponent();
+        }
+
+        public void Init(ShopBC xm_ShopBC, DBTablesAndColumnNames xDBtcn)
+        {
+            m_ShopBC = xm_ShopBC;
+            DBtcn = xDBtcn;
+        }
+
+    }
+}
