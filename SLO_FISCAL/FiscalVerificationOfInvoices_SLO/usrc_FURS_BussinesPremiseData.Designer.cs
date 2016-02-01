258c7f2fc52d9f0ddcfa4abca8135b0c9616f658
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2016-02-01 23:35:00 +0100

    FIRST INVOICE SENT TO FURS  SUCCESS!

--- SLO_FISCAL/FiscalVerificationOfInvoices_SLO/usrc_FURS_BussinesPremiseData.Designer.cs
+++ SLO_FISCAL/FiscalVerificationOfInvoices_SLO/usrc_FURS_BussinesPremiseData.Designer.cs
@@ -51,18 +51,20 @@
             this.lbl_BussinesPremiseID = new System.Windows.Forms.Label();
             this.btn_ImportFromDataBase = new System.Windows.Forms.Button();
             this.Btn_Add_PP_to_FURS = new System.Windows.Forms.Button();
+            this.txt_InvoiceAuthor_TaxID = new System.Windows.Forms.TextBox();
+            this.lbl_InvoiceAuthor_TaxID = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_BuildingNumber
             // 
-            this.txt_BuildingNumber.Location = new System.Drawing.Point(144, 53);
+            this.txt_BuildingNumber.Location = new System.Drawing.Point(504, 74);
             this.txt_BuildingNumber.Name = "txt_BuildingNumber";
-            this.txt_BuildingNumber.Size = new System.Drawing.Size(91, 20);
+            this.txt_BuildingNumber.Size = new System.Drawing.Size(125, 20);
             this.txt_BuildingNumber.TabIndex = 18;
             // 
             // lbl_BuildingNumber
             // 
-            this.lbl_BuildingNumber.Location = new System.Drawing.Point(5, 56);
+            this.lbl_BuildingNumber.Location = new System.Drawing.Point(348, 81);
             this.lbl_BuildingNumber.Name = "lbl_BuildingNumber";
             this.lbl_BuildingNumber.Size = new System.Drawing.Size(133, 13);
             this.lbl_BuildingNumber.TabIndex = 17;
@@ -79,14 +81,14 @@
             // 
             // txt_BuildingSectionNumber
             // 
-            this.txt_BuildingSectionNumber.Location = new System.Drawing.Point(537, 71);
+            this.txt_BuildingSectionNumber.Location = new System.Drawing.Point(504, 101);
             this.txt_BuildingSectionNumber.Name = "txt_BuildingSectionNumber";
-            this.txt_BuildingSectionNumber.Size = new System.Drawing.Size(92, 20);
+            this.txt_BuildingSectionNumber.Size = new System.Drawing.Size(125, 20);
             this.txt_BuildingSectionNumber.TabIndex = 21;
             // 
             // lbl_BuildingSectionNumber
             // 
-            this.lbl_BuildingSectionNumber.Location = new System.Drawing.Point(361, 74);
+            this.lbl_BuildingSectionNumber.Location = new System.Drawing.Point(348, 104);
             this.lbl_BuildingSectionNumber.Name = "lbl_BuildingSectionNumber";
             this.lbl_BuildingSectionNumber.Size = new System.Drawing.Size(135, 13);
             this.lbl_BuildingSectionNumber.TabIndex = 20;
@@ -95,14 +97,14 @@
             // 
             // txt_Community
             // 
-            this.txt_Community.Location = new System.Drawing.Point(144, 79);
+            this.txt_Community.Location = new System.Drawing.Point(504, 127);
             this.txt_Community.Name = "txt_Community";
-            this.txt_Community.Size = new System.Drawing.Size(166, 20);
+            this.txt_Community.Size = new System.Drawing.Size(125, 20);
             this.txt_Community.TabIndex = 23;
             // 
             // lbl_Community
             // 
-            this.lbl_Community.Location = new System.Drawing.Point(5, 82);
+            this.lbl_Community.Location = new System.Drawing.Point(348, 130);
             this.lbl_Community.Name = "lbl_Community";
             this.lbl_Community.Size = new System.Drawing.Size(133, 13);
             this.lbl_Community.TabIndex = 22;
@@ -111,14 +113,14 @@
             // 
             // txt_CadastralNumber
             // 
-            this.txt_CadastralNumber.Location = new System.Drawing.Point(537, 101);
+            this.txt_CadastralNumber.Location = new System.Drawing.Point(504, 48);
             this.txt_CadastralNumber.Name = "txt_CadastralNumber";
-            this.txt_CadastralNumber.Size = new System.Drawing.Size(92, 20);
+            this.txt_CadastralNumber.Size = new System.Drawing.Size(125, 20);
             this.txt_CadastralNumber.TabIndex = 25;
             // 
             // lbl_CadastralNumber
             // 
-            this.lbl_CadastralNumber.Location = new System.Drawing.Point(361, 104);
+            this.lbl_CadastralNumber.Location = new System.Drawing.Point(350, 55);
             this.lbl_CadastralNumber.Name = "lbl_CadastralNumber";
             this.lbl_CadastralNumber.Size = new System.Drawing.Size(133, 13);
             this.lbl_CadastralNumber.TabIndex = 24;
@@ -127,14 +129,15 @@
             // 
             // dt_ValidityDate
             // 
-            this.dt_ValidityDate.Location = new System.Drawing.Point(144, 102);
+            this.dt_ValidityDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt_ValidityDate.Location = new System.Drawing.Point(204, 100);
             this.dt_ValidityDate.Name = "dt_ValidityDate";
-            this.dt_ValidityDate.Size = new System.Drawing.Size(166, 20);
+            this.dt_ValidityDate.Size = new System.Drawing.Size(107, 20);
             this.dt_ValidityDate.TabIndex = 26;
             // 
             // lbl_ValidityDate
             // 
-            this.lbl_ValidityDate.Location = new System.Drawing.Point(5, 108);
+            this.lbl_ValidityDate.Location = new System.Drawing.Point(4, 100);
             this.lbl_ValidityDate.Name = "lbl_ValidityDate";
             this.lbl_ValidityDate.Size = new System.Drawing.Size(107, 13);
             this.lbl_ValidityDate.TabIndex = 27;
@@ -143,30 +146,31 @@
             // 
             // txt_ClosingTag
             // 
-            this.txt_ClosingTag.Location = new System.Drawing.Point(434, 134);
+            this.txt_ClosingTag.Location = new System.Drawing.Point(124, 134);
             this.txt_ClosingTag.Name = "txt_ClosingTag";
             this.txt_ClosingTag.Size = new System.Drawing.Size(26, 20);
             this.txt_ClosingTag.TabIndex = 29;
             // 
             // lbl_ClosingTag
             // 
-            this.lbl_ClosingTag.Location = new System.Drawing.Point(330, 138);
+            this.lbl_ClosingTag.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_ClosingTag.Location = new System.Drawing.Point(8, 134);
             this.lbl_ClosingTag.Name = "lbl_ClosingTag";
             this.lbl_ClosingTag.Size = new System.Drawing.Size(98, 13);
             this.lbl_ClosingTag.TabIndex = 28;
             this.lbl_ClosingTag.Text = "lbl_ClosingTag";
-            this.lbl_ClosingTag.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbl_ClosingTag.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // txt_SoftwareSupplier_TaxNumber
             // 
-            this.txt_SoftwareSupplier_TaxNumber.Location = new System.Drawing.Point(176, 169);
+            this.txt_SoftwareSupplier_TaxNumber.Location = new System.Drawing.Point(203, 74);
             this.txt_SoftwareSupplier_TaxNumber.Name = "txt_SoftwareSupplier_TaxNumber";
-            this.txt_SoftwareSupplier_TaxNumber.Size = new System.Drawing.Size(134, 20);
+            this.txt_SoftwareSupplier_TaxNumber.Size = new System.Drawing.Size(107, 20);
             this.txt_SoftwareSupplier_TaxNumber.TabIndex = 31;
             // 
             // lbl_SoftwareSupplier_TaxNumber
             // 
-            this.lbl_SoftwareSupplier_TaxNumber.Location = new System.Drawing.Point(5, 172);
+            this.lbl_SoftwareSupplier_TaxNumber.Location = new System.Drawing.Point(5, 77);
             this.lbl_SoftwareSupplier_TaxNumber.Name = "lbl_SoftwareSupplier_TaxNumber";
             this.lbl_SoftwareSupplier_TaxNumber.Size = new System.Drawing.Size(242, 13);
             this.lbl_SoftwareSupplier_TaxNumber.TabIndex = 30;
@@ -175,14 +179,14 @@
             // 
             // txt_PremiseType
             // 
-            this.txt_PremiseType.Location = new System.Drawing.Point(600, 131);
+            this.txt_PremiseType.Location = new System.Drawing.Point(282, 134);
             this.txt_PremiseType.Name = "txt_PremiseType";
             this.txt_PremiseType.Size = new System.Drawing.Size(29, 20);
             this.txt_PremiseType.TabIndex = 33;
             // 
             // lbl_PremiseType
             // 
-            this.lbl_PremiseType.Location = new System.Drawing.Point(501, 137);
+            this.lbl_PremiseType.Location = new System.Drawing.Point(177, 137);
             this.lbl_PremiseType.Name = "lbl_PremiseType";
             this.lbl_PremiseType.Size = new System.Drawing.Size(87, 13);
             this.lbl_PremiseType.TabIndex = 32;
@@ -191,9 +195,9 @@
             // 
             // txt_MyOrganisation_TaxID
             // 
-            this.txt_MyOrganisation_TaxID.Location = new System.Drawing.Point(144, 25);
+            this.txt_MyOrganisation_TaxID.Location = new System.Drawing.Point(203, 22);
             this.txt_MyOrganisation_TaxID.Name = "txt_MyOrganisation_TaxID";
-            this.txt_MyOrganisation_TaxID.Size = new System.Drawing.Size(65, 20);
+            this.txt_MyOrganisation_TaxID.Size = new System.Drawing.Size(107, 20);
             this.txt_MyOrganisation_TaxID.TabIndex = 35;
             this.txt_MyOrganisation_TaxID.Text = "12345678";
             // 
@@ -210,14 +214,14 @@
             // 
             this.txt_BussinesPremiseID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_BussinesPremiseID.Location = new System.Drawing.Point(488, 19);
+            this.txt_BussinesPremiseID.Location = new System.Drawing.Point(504, 19);
             this.txt_BussinesPremiseID.Name = "txt_BussinesPremiseID";
-            this.txt_BussinesPremiseID.Size = new System.Drawing.Size(141, 20);
+            this.txt_BussinesPremiseID.Size = new System.Drawing.Size(125, 20);
             this.txt_BussinesPremiseID.TabIndex = 37;
             // 
             // lbl_BussinesPremiseID
             // 
-            this.lbl_BussinesPremiseID.Location = new System.Drawing.Point(361, 22);
+            this.lbl_BussinesPremiseID.Location = new System.Drawing.Point(348, 22);
             this.lbl_BussinesPremiseID.Name = "lbl_BussinesPremiseID";
             this.lbl_BussinesPremiseID.Size = new System.Drawing.Size(192, 13);
             this.lbl_BussinesPremiseID.TabIndex = 36;
@@ -243,10 +247,28 @@
             this.Btn_Add_PP_to_FURS.UseVisualStyleBackColor = true;
             this.Btn_Add_PP_to_FURS.Click += new System.EventHandler(this.Btn_Add_PP_to_FURS_Click);
             // 
+            // txt_InvoiceAuthor_TaxID
+            // 
+            this.txt_InvoiceAuthor_TaxID.Location = new System.Drawing.Point(203, 48);
+            this.txt_InvoiceAuthor_TaxID.Name = "txt_InvoiceAuthor_TaxID";
+            this.txt_InvoiceAuthor_TaxID.Size = new System.Drawing.Size(107, 20);
+            this.txt_InvoiceAuthor_TaxID.TabIndex = 43;
+            // 
+            // lbl_InvoiceAuthor_TaxID
+            // 
+            this.lbl_InvoiceAuthor_TaxID.Location = new System.Drawing.Point(5, 51);
+            this.lbl_InvoiceAuthor_TaxID.Name = "lbl_InvoiceAuthor_TaxID";
+            this.lbl_InvoiceAuthor_TaxID.Size = new System.Drawing.Size(192, 13);
+            this.lbl_InvoiceAuthor_TaxID.TabIndex = 42;
+            this.lbl_InvoiceAuthor_TaxID.Text = "lbl_InvoiceAuthor_TaxID";
+            this.lbl_InvoiceAuthor_TaxID.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // usrc_FURS_BussinesPremiseData
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.txt_InvoiceAuthor_TaxID);
+            this.Controls.Add(this.lbl_InvoiceAuthor_TaxID);
             this.Controls.Add(this.Btn_Add_PP_to_FURS);
             this.Controls.Add(this.btn_ImportFromDataBase);
             this.Controls.Add(this.txt_BussinesPremiseID);
@@ -302,5 +324,7 @@
         private System.Windows.Forms.Label lbl_BussinesPremiseID;
         private System.Windows.Forms.Button btn_ImportFromDataBase;
         private System.Windows.Forms.Button Btn_Add_PP_to_FURS;
+        private System.Windows.Forms.TextBox txt_InvoiceAuthor_TaxID;
+        private System.Windows.Forms.Label lbl_InvoiceAuthor_TaxID;
     }
 }
