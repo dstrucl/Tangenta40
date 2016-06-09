372a3ab539da2393f3723681f0e615e4488cfcc5
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2016-06-09 13:14:33 +0200

    f_SimplItem Added

---
+++ TANGENTA/TangentaDB/f_SimpleItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBTypes;
+
+namespace TangentaDB
+{
+    public static class f_SimpleItem
+    {
+        public static bool Get(string Name, string Abbreviation,bool bToOffer,long_v SimpleItem_Image_ID_v,int_v Code_v,long_v SimpleItem_ParentGroup1_ID_v)
+        {
+            return false;
+        }
+    }
+}
