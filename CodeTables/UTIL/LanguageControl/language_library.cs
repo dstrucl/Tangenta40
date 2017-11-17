b5b12191262e2b1fcc8780cebce04e143c6ae6ae
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2017-11-17 21:53:05 +0100

    Language_Library.cs Added

---
+++ CodeTables/UTIL/LanguageControl/language_library.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LanguageControl
+{
+    public class language_library
+    {
+        private string module_name = null;
+        public string ModuleName
+        {
+            get { return module_name; }
+            set { module_name = value; }
+        }
+
+        private FieldInfo[] fields = null;
+        public FieldInfo[] Fields
+        {
+            get { return fields; }
+            set { fields = value; }
+        }
+      
+        public language_library(string xModuleName, FieldInfo[] xFields)
+        {
+            ModuleName = xModuleName;
+            Fields = xFields;
+        }
+    }
+}
