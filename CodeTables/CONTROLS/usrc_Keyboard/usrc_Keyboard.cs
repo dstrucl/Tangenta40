eedee7d225484300d4d16807332f907a98f7ccb8
Damjan Štrucl-Hrnčič <dstrucl@gmail.com>
2018-10-06 17:07:08 +0200

    Add usrc_Keyboard

---
+++ CodeTables/CONTROLS/usrc_Keyboard/usrc_Keyboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace usrc_Keyboard
+{
+    public partial class usrc_Keyboard : UserControl
+    {
+        public usrc_Keyboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
